2e84bfd6f3e12e034e8e4d920ed53b03e3f60c99
j-rods <jrrodriguez84@gmail.com>
2018-09-20 18:27:41 +0800

    set up relationships between bit, like, reply and user

--- app/Bit.php
+++ app/Bit.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bit extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function bit()
+    {
+        return $this->belongsTo(Bit::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
