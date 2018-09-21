5cb0915a92eb667aae1f0df1f5e187f4937461b6
j-rods <jrrodriguez84@gmail.com>
2018-09-21 09:41:29 +0800

    add type fields in user type

--- app/GraphQL/Type/UserType.php
+++ app/GraphQL/Type/UserType.php
@@ -4,5 +4,33 @@ namespace App\GraphQL\Type;
 
 class UserType extends GraphQLType
 {
-
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of a user'
+            ],
+            'name' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The name of a user'
+            ],
+            'email' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The email of a user'
+            ],
+            'bits' => [
+                'type' => Type::listOf(GraphQL::type('Bit')),
+                'description' => 'The user bits'
+            ],
+            'created_at' => [
+                'type' => Type::string(),
+                'description' => 'Date it was created'
+            ],
+            'updated_at' => [
+                'type' => Type::string(),
+                'description' => 'Date it was updated'
+            ]
+        ];
+    }
 }
